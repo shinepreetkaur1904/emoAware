42fc7b419631c34831b8ccb84c04737cc9907f2c
shine <149325041+shinepreetkaur1904@users.noreply.github.com>
2025-11-10 21:31:24 +0530

    fix game links

--- scripts/game.js
+++ scripts/game.js
@@ -1,23 +1,23 @@
 function openGame(type) {
   switch(type) {
     case 'bubble':
-      window.location.href = "../games/bubble-pop.html";
+      window.location.href = "games/bubble-pop.html";
       break;
 
     case 'breath':
-      window.location.href = "../games/breathing.html";
+      window.location.href = "games/breathing.html";
       break;
 
     case 'memory':
-      window.location.href = "../games/memory-tiles.html";
+      window.location.href = "games/memory-tiles.html";
       break;
 
     case 'paint':
-      window.location.href = "../games/mood-paint.html";
+      window.location.href = "games/mood-paint.html";
       break;
 
     case 'music':
-      window.location.href = "../games/calm-music.html";
+      window.location.href = "games/calm-music.html";
       break;
 
     default:
